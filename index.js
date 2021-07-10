b58dc3ea9d9709f400b9ba21a57a145371972a8c
hyewoncc <hyewoncc@gmail.com>
2021-07-10 15:17:21 +0900

    설정 파일 숨김처리

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require('./model/User');
+
+const config = require('./config/key');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyewon:1225@boilerplate.tq84y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! 안녕안녕'))
+app.get('/', (req, res) => res.send('Hello World! 안녕안녕, 날씨가 좋네'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+    // 그것들을 DB에 넣어준다 
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({sucess: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
